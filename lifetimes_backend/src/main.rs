8038663d148855f9dd16f7688cdf959ac089646d
Florian Seligmann <floseligmann@web.de>
2022-01-28 17:47:50 +0100

    tests

--- lifetimes_backend/src/main.rs
+++ lifetimes_backend/src/main.rs
@@ -1,9 +1,10 @@
 use std::sync::Arc;
 
-use base_db::{Env, CrateOrigin};
-use hir::CfgOptions;
+use base_db::{Env, CrateOrigin, FilePosition};
+use hir::{CfgOptions, Semantics};
 use ide::{AnalysisHost, Change, FileId, CrateGraph, SourceRoot, DiagnosticsConfig, AssistResolveStrategy, Edition};
 use vfs::{file_set::FileSet, VfsPath};
+use syntax::ast::AstNode;
 
 fn main() {
     let mut host = AnalysisHost::new(None);
@@ -20,11 +21,23 @@ fn main() {
     let mut initial_change = Change::default();
     initial_change.set_roots(vec![root]);
     initial_change.set_crate_graph(crate_graph);
-    initial_change.change_file(FileId(0), Some(Arc::new("fn main() {}".to_string())));
+    initial_change.change_file(FileId(0), Some(Arc::new(r#"fn main() {
+        let mut x = 5;
+        let y = &mut x;
+        *y = 3;
+        x = 4;
+    }"#.to_string())));
 
     host.apply_change(initial_change);
 
     let analysis = host.analysis();
-    println!("{:?}", analysis.file_text(FileId(0)));
+    println!("{:?}", analysis.file_text(file));
+    println!("{:?}", analysis.view_hir(FilePosition {file_id: file, offset: 0.into()}));
     println!("{:?}", analysis.diagnostics(&DiagnosticsConfig::default(), AssistResolveStrategy::All, file));
+
+    let semantics = Semantics::new(host.raw_database());
+    let file_node = semantics.parse(file);
+    let ast = file_node.syntax();
+    semantics.descend_into_macros();
+    dbg!(ast);
 }
